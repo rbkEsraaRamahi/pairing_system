f8f234e506567e6129241b8b8cbbcb48783bb9fe
ZoulfaALBawab <zoulfaalbawab90@gmail.com>
2018-05-05 13:59:24 +0300

    added the client main files

---
+++ containers/App.js
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render () {
+    	return (<div>
+      Student name:<input/>       
+      <button> Add Name </button>
+    </div>) 
+        
+
+    }
+}
