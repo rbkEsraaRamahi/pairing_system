d4f5b0f3f786d3327039e5fca71b98aab8957f92
rbkEsraaRamahi <israma7i@yahoo.com>
2018-05-07 15:13:56 +0300

    main list

--- client/src/components/pairingList.jsx
+++ client/src/components/pairingList.jsx
@@ -1,8 +1,13 @@
 import React from 'react';
-
+import App from '../index';
 const Pairing = (props) => (
   <div>
     <h4> Pairing List </h4>
+    <ul>
+    {props.students.map(student =>
+      <li> {student} </li>
+    )}
+  </ul>
   </div>
 )
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -7,7 +7,7 @@ class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      students: []
+      students: ["Esraa", "Hanan", "Sahar", "Mohannad", "Mai", "Heba"]
     }
 
   }
@@ -23,14 +23,21 @@ class App extends React.Component {
       }
     });
   }
+  pairingList(data){
+    console.log('sucesss');
+
+  }
 
   render () {
     return (<div>
       <h1>Pairing System</h1>
-      <Student onAdd={this.add.bind(this)}/>
+      
       <Pairing students={this.state.students}/>
+     <button onClick={this.pairingList}> create </button>
+
+      
     </div>)
   }
 }
 
-ReactDOM.render(<App />, document.getElementById('app'));
+export default App;
