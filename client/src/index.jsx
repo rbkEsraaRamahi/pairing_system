7c7981eac32b94a511f9f539bee57e1cad857605
rbkEsraaRamahi <israma7i@yahoo.com>
2018-05-08 16:06:58 +0300

    basic random fxn front end

--- client/src/index.jsx
+++ client/src/index.jsx
@@ -3,16 +3,21 @@ import ReactDOM from 'react-dom';
 import Student from './components/addStudent.jsx';
 import Pairing from './components/pairingList.jsx';
 
+
 class App extends React.Component {
   constructor(props) {
     super(props);
     this.state = { 
-      students: ["Esraa", "Hanan", "Sahar", "Mohannad", "Mai", "Heba"]
+      students: ["Esraa", "Hanan", "Sahar", "Mohannad", "Mai", "Heba"],
+      pairing:[]
     }
+    this.pairingList = this.pairingList.bind(this);
+
 
   }
 
   add (term) {
+  
     $.ajax({
       type: "POST",
       url: "/",
@@ -22,20 +27,42 @@ class App extends React.Component {
       }
     });
   }
-  pairingList(data){
-    console.log('sucesss');
-
+  pairingList(){
+    var arr = this.state.students;
+    var length = arr.length;
+      for (var i =0; i < arr.length; i++) {
+        var rand = Math.floor(Math.random()*(length-i)+i);
+        var previos = arr[i];
+        arr[i] = arr[rand];
+        arr[rand] = previos; 
+      }
+      this.state.pairing=arr;
+  this.setState({
+        pairing: this.state.pairing
+    })
+    console.log(this.state.pairing);
   }
 
   render () {
     return (<div>
       <h1>Pairing System</h1>
-      
-      <Pairing students={this.state.students}/>
-     <button onClick={this.pairingList}> create </button>
+      <Student/>
+      <h4> Pairing List </h4>
+    <button onClick={this.pairingList}> create </button>
+
+    {this.state.pairing.map(student =>
+      <table>
+        <tr>
+          <th>{student}</th>
+        </tr>
+      </table>
+    )}
+  
+     
 
       
     </div>)
   }
 }
-export default App
+
+export default App;
