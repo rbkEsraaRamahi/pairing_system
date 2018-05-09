083b3f70664685c196e21fde54121aa2294b89c4
mohannadbakkar <mohannadbakkar@gmail.com>
2018-05-09 15:23:53 +0300

    added controller updateOne and routr for it

--- server/db/controller.js
+++ server/db/controller.js
@@ -12,3 +12,45 @@ exports.createOne = function (req, res) {
   });
  
 };
+
+exports.retrieve = function(req, res){
+	Student.find().find(function(err, data){
+		if(err){
+			return res.status(500).json(err.message)
+		}else{
+			res.status(201).json(data)
+		}
+	})
+};
+
+exports.updateOne = function(req,res){
+	var students = JSON.parse(req.body.student)
+	Student.find().find(function(err,data){
+		for (var i = 0; i < data.length; i++) {
+			for (var j = 0; j < students.length; j++) {
+				if(data[i].StudentName === students[j][0]){
+					data[i].WhoPairedWith.push(students[j][1])
+					Student.findOneAndUpdate({StudentName: students[j][0]}, {$set:{WhoPairedWith: data[i].WhoPairedWith}}, function(err,data){
+						if (err){
+							// res.status(500).json(err.message);
+						}else{
+							res.end(JSON.stringify(data))
+						}
+					})
+				}if (data[i].StudentName === students[j][1]){
+					data[i].WhoPairedWith.push(students[j][0])
+					Student.findOneAndUpdate({StudentName: students[j][1]}, {$set:{WhoPairedWith: data[i].WhoPairedWith}}, function(err,data){
+						if (err){
+							// res.status(500).json(err.message);
+						}else{
+							res.end(JSON.stringify(data))
+					}
+					})
+				}
+					
+			}
+		}
+	})
+}
+
+
--- server/db/db.js
+++ server/db/db.js
@@ -1,7 +1,7 @@
 var mongoose = require('mongoose');
 
 mongoose.connect('mongodb://localhost/pairing');
-
+mongoose.set('debug', true);
 var db = mongoose.connection
 	
 db.on('error', console.error.bind(console, 'connection error:'));
