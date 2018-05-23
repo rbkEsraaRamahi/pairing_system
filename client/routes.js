57d6369ed4c238779a7873b5631d31dca7696140
abdulazeeznaji <abdulazeeznaji@gmail>
2018-05-23 03:29:22 +0300

    Add Groups ui and wrap the whole application with AppBar

--- client/routes.js
+++ client/routes.js
@@ -6,18 +6,20 @@ import {Router, Route, IndexRoute, browserHistory, browserHistory as history} fr
 import Dashboard from './src/index'
 import Pairs from './src/components/Pairs'
 import Student from './src/components/addStudent'
+import Groups from './src/components/Groups'
+import Pairing from './src/components/pairingList.jsx';
 
 const routes = (
   <Router history={browserHistory}>
-    <Route path='/' component={Dashboard} />
+    <Route path='/' component={Dashboard}>
     <Route path='/student' component={Student} />
-    <Route path='/groups' component={Pairs} />
-    <Route path='/pairs' component={null} />
-    <Route path='/reports' component={null} />
+    <Route path='/pairing' component={Pairing} />
+    <Route path='/pairs' component={Pairs} />
+    <Route path='/groups' component={Groups} />
     <Route path='/assessments' component={null} />
     <Route path='/cohorts' component={null} />
     <Route path='/projects' component={null} />
-
+    </Route>
   </Router>
 )
 
--- client/src/index.jsx
+++ client/src/index.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { browserHistory as history } from 'react-router';
-import Pairing from './components/pairingList.jsx';
+
 import RaisedButton from 'material-ui/RaisedButton';
 import TextField from 'material-ui/TextField';
 import Drawer from 'material-ui/Drawer';
@@ -26,137 +26,27 @@ class App extends React.Component {
     }
   }
 
-  add () {
-    var that = this
-    console.log(this.state.pairing)
-    $.ajax({
-      type: 'PUT',
-      url: 'http://localhost:3000/api/student/update',
-      data: {'student': that.state.pairing},
-      success: function (res) {
-        console.log('sucesss')
-      }
-    })
-  }
-  pairingList () {
-    var that = this
-    $.ajax({
-      type: 'GET',
-      url: ' http://localhost:3000/api/student/',
-      data: {},
-      success: function (dataB) {
-        function randomaize (data) {
-          var arr = []
-          for (var i = 0; i < data.length; i++) {
-            arr.push(data[i])
-          }
-          var length = arr.length
-          for (var i = 0; i < arr.length; i++) {
-            var rand = Math.floor(Math.random() * (length - i) + i)
-            var previos = arr[i]
-            arr[i] = arr[rand]
-            arr[rand] = previos
-          }
-          return arr
-        }
-        var arr = randomaize(dataB)
-        var result = []
-        var counter = -1
-        var filter = []
-        if (arr.length % 2 !== 0) {
-          counter = 0
-          for (var i = 0; i < arr.length; i++) {
-            if (arr[i].Level >= 3.5 && !arr[i].WhoPairedWith.includes('DUCK')) {
-              result.push([arr[i], {StudentName: 'DUCK'}])
-              arr.splice(i, 1)
-              break
-            }
-          }
-        }
-        while (arr.length > 0) {
-          counter++
-          result.push([arr[0]])
-          var correntStudent = arr.shift()
-          for (var i = 0; i < arr.length; i++) {
-            if (arr[i].Level - 1 === correntStudent.Level || arr[i].Level + 1 === correntStudent.Level) {
-              if (correntStudent.WhoPairedWith.indexOf(arr[i].StudentName) === -1) {
-                result[counter].push(arr[i])
-                var index = arr.indexOf(arr[i])
-                arr.splice(index, 1)
-                break
-              }
-            }
-          }
-          for (var i = 0; i < result.length; i++) {
-            if (result[i].length === 1) {
-              for (var j = i; j < result.length; j++) {
-                if (result[j] !== result[i] && result[j].length === 1) {
-                  if (result[i][0].WhoPairedWith.indexOf(result[j][0].StudentName) === -1) {
-                    result[i].push(result[j][0])
-                    result.splice(j, 1)
-                  }
-                }
-              }
-            }
-          }
-        }
-        that.state.pairing = result
-        that.setState({
-          pairing: that.state.pairing
-        })
-      },
-      error: function (request, status, error) {
-        console.log(error)
-      }
-    })
-  }
+
   render () {
-    var usernameComp = (
-        <div><h1> Pairing List </h1>
-          <form onSubmit={this.handleSubmit}>
-            <label style={{fontWeight: 'bold', fontSize: '20px'}}> Enter Sprint Name :   </label>
-            <TextField floatingLabelText='Enter Sprint Name' onChange={this.change} />
-          </form>
-          <RaisedButton label='Create' buttonStyle={{ background: '#FF1493'}} onClick={this.pairingList} />
-          <Table>
-            <TableHeader>
-              <TableRow>
-                <TableHeaderColumn>Student1</TableHeaderColumn>
-                <TableHeaderColumn>Level1</TableHeaderColumn>
-                <TableHeaderColumn>Student2</TableHeaderColumn>
-                <TableHeaderColumn>Level2</TableHeaderColumn>
-              </TableRow>
-            </TableHeader>
-            <TableBody>
-              {this.state.pairing.map((student, index) =>
-                  <TableRow>
-                    <TableRowColumn>{student[0].StudentName}</TableRowColumn>
-                    <TableRowColumn>{student[0].Level}</TableRowColumn>
-                    <TableRowColumn>{student[1].StudentName}</TableRowColumn>
-                    <TableRowColumn>{student[1].Level}</TableRowColumn>
-                  </TableRow>
-              )}
-            </TableBody>
-          </Table>
-          <RaisedButton label='Submit' buttonStyle={{ background: '#FF1493'}} onClick={this.add} /></div>
-    )
     return (
         <div>
+
           <Drawer open={this.state.open}>
-            <MenuItem onClick={()=> history.push('./groups')}> Groups </MenuItem>
-            <MenuItem onClick={()=> history.push('./pairs')}> Pairs </MenuItem>
-            <MenuItem onClick={()=> history.push('./assessments')}> Assessments </MenuItem>
-            <MenuItem onClick={()=> history.push('./reports')}> Reports </MenuItem>
-            <MenuItem onClick={()=> history.push('./cohorts')}> Cohorts </MenuItem>
-            <MenuItem onClick={()=> history.push('./Projects')}> projects </MenuItem>
+            <MenuItem onClick={()=> history.push('/groups')}> Groups </MenuItem>
+            <MenuItem onClick={()=> history.push('/pairs')}> Pairs </MenuItem>
+            <MenuItem onClick={()=> history.push('/pairing')}> Pairing </MenuItem>
+            <MenuItem onClick={()=> history.push('/assessments')}> Assessments </MenuItem>
+            <MenuItem onClick={()=> history.push('/reports')}> Reports </MenuItem>
+            <MenuItem onClick={()=> history.push('/cohorts')}> Cohorts </MenuItem>
+            <MenuItem onClick={()=> history.push('/Projects')}> projects </MenuItem>
           </Drawer>
           <AppBar onClick={() => this.setState({open: !this.state.open})} title='Pairing System' style={{background: '#FF1493', fontWeight: 'bold'}} >
             <Tabs>
-              <Tab label='&nbsp;Add&nbsp;Students&nbsp;&nbsp;&nbsp;&nbsp;'style={{background: '#FF1493', fontWeight: 'bold'}} onClick={() => history.push('/addStudent')} />
+              <Tab label='&nbsp;Add&nbsp;Students&nbsp;&nbsp;&nbsp;&nbsp;'style={{background: '#FF1493', fontWeight: 'bold'}} onClick={() => history.push('/student')} />
               <Tab label='&nbsp;Groups&nbsp;' style={{background: '#FF1493', fontWeight: 'bold'}} />
             </Tabs>
           </AppBar>
-          <Pairing />
+             {this.props.children}
         </div>
     )
   }
